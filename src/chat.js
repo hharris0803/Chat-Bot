08e32c10f9e99d63946d8f96211f8f19d7e56b87
hharris0803 <hchang0803@outlook.com>
2018-12-07 09:15:01 -0500

    delete unnecessary files, modifications to app.js(reducer, middleware) and  chat.js(feed, sendMessage)

--- src/chat.js
+++ src/chat.js
@@ -1,4 +1,5 @@
 import {ApiAiClient} from 'api-ai-javascript';
+import { applyMiddleware,createStore } from 'redux';
 
 const accessToken = '0a687f42dd984521a304d63538109cbd';
 const client = new ApiAiClient({accessToken})
@@ -12,10 +13,24 @@ export const sendMessage = (text, sender='user') => ({
 });
 
 const messageMiddleware = () => next => action => {
+    next(action);
 
+    if(action.type === ON_MESSAGE) {
+        const { text } = action.payload;
+
+        client.textRequest(text)
+        .then( onSuccess )
+
+        function onSuccess(response) {
+            const {result: {fulfillment}} = response;
+            next(sendMessage(fulfillment.speech, 'bot'));
+        }
+    }
 };
 
-const messageReducer = (state = [], action) => {
+const initState = [{ text: 'hey' }];
+
+const messageReducer = (state = initState, action) => {
     switch (action.type) {
         
         case ON_MESSAGE:
@@ -25,3 +40,5 @@ const messageReducer = (state = [], action) => {
         return state;
     }
 };
+
+export const store = createStore(messageReducer, applyMiddleware(messageMiddleware));
