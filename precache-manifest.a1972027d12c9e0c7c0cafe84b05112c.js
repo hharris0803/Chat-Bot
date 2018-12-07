636ebdec1597804aecf8374f4d1ae3448d6f6790
hharris0803 <hchang0803@outlook.com>
2018-12-07 16:07:10 -0500

    Updates

---
+++ precache-manifest.a1972027d12c9e0c7c0cafe84b05112c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dab0e737c060cc51ced3",
+    "url": "/Chat-Bot/static/js/main.dab0e737.chunk.js"
+  },
+  {
+    "revision": "845bcedb68fd45d06db9",
+    "url": "/Chat-Bot/static/css/1.26b31052.chunk.css"
+  },
+  {
+    "revision": "845bcedb68fd45d06db9",
+    "url": "/Chat-Bot/static/js/1.845bcedb.chunk.js"
+  },
+  {
+    "revision": "3ed921dc393757b854f1",
+    "url": "/Chat-Bot/static/js/runtime~main.3ed921dc.js"
+  },
+  {
+    "revision": "9a4868bde8421fcf3b87860bfb1c8300",
+    "url": "/Chat-Bot/index.html"
+  }
+];
